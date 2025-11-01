3a5ebca8fa4e534c47a09f8368777b58466f2bd6
Nagarajan Ravikumar <rnagarajanmca@gmail.com>
2025-11-01 18:53:08 -0500

    Adding deps for vercel analytics

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from 'next';
 
 const config: NextConfig = {
   reactStrictMode: true,
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default config;
